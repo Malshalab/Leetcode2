35c198d8d13de9f407007f8c27b78d8917ba0b39
Mohammad Al-Shalabi <91227088+Malshalab@users.noreply.github.com>
2025-05-13 15:54:51 -0400

    Create Merge_Two_Sorted_Lists.java

---
+++ Merge_Two_Sorted_Lists.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy= new ListNode(0) ;
+        ListNode value= dummy ;
+
+        while(list1!=null && list2!=null){
+            if(list1.val>list2.val){
+                value.next=list2 ;
+                list2=list2.next ;
+            }
+            else {
+                value.next=list1 ;
+                list1=list1.next ;
+            }
+            
+            value=value.next ;
+
+        }
+
+        if(list1!=null){
+            value.next=list1 ;
+        }
+        else{
+            value.next=list2 ;
+        }
+        return dummy.next ;
+    }
+}
